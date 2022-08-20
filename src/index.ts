6bc91f77cd9dfa40b65950c63ddd43751d07094e
Ghif Aliy <ontajenius@yahoo.co.id>
2022-08-20 19:31:20 +0700

    fix(): change to play-dl because unreliable ytdl

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Player, Queue } from "discord-player";
-import { Client, CommandInteraction, GatewayIntentBits, Guild, GuildMember } from "discord.js";
+import { Client, CommandInteraction, GatewayIntentBits, Guild, GuildMember, Interaction, VoiceBasedChannel } from "discord.js";
 import "dotenv/config";
+import { pino } from "pino";
+import * as playdl from "play-dl";
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
+});
+
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates],
 });
@@ -10,17 +17,38 @@ client.login(process.env.DISCORD_TOKEN);
 // Create a new Player (you don't need any API Key)
 const player = new Player(client);
 
-player.on("trackStart", (queue, track) => console.log(typeof queue.metadata));
+player.on("trackStart", async (queue, track) => {
+  if (!queue.metadata) throw "metadata not defined";
+  const metadata = queue.metadata as Interaction;
+  await metadata.channel?.send(`Now playing **${track.title}**!`);
+  logger.info({ track }, "trackStart");
+});
+
+player.on("trackAdd", (queue, track) => logger.debug({ track }, "trackAdd"));
+
+player.on("trackEnd", (queue, track) => logger.debug({ track }, "trackEnd"));
+
+player.on("connectionCreate", (queue, stream) => logger.debug({ stream }, "connectionCreate"));
+
+player.on("connectionError", (queue, err) => logger.error({ err }, "connectionError"));
+
+player.on("debug", (queue, data) => logger.debug({ data }, "debug"));
+
+player.on("queueEnd", (queue) => logger.debug("queueEnd"));
+
+player.on("channelEmpty", (queue) => logger.debug("channelEmpty"));
+
+player.on("tracksAdd", (queue) => logger.debug("tracksAdd"));
 
 client.once("ready", () => {
-  console.log(`Logged in as ${client.user?.tag}`);
+  logger.info(`Logged in as ${client.user?.tag}`);
 });
 
 player.on("error", (queue, error) => {
-  console.error("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-  console.error(typeof queue.metadata);
-  // queue.metadata.channel.send(`Adu maap error nih, antara lagunya error ato yang bikin emang bloon`);
-  console.log({ message: error.message, detail: error.name, stack: error.stack });
+  if (!queue.metadata) throw "metadata not defined";
+  const metadata = queue.metadata as Interaction;
+  metadata.channel?.send(`Adu maap error nih, antara lagunya error ato yang bikin emang bloon`);
+  logger.error({ message: error.message, detail: error.name, stack: error.stack });
 });
 
 client.on("interactionCreate", async (interaction) => {
@@ -28,8 +56,15 @@ client.on("interactionCreate", async (interaction) => {
 
   // play music
   if (interaction.commandName === "setel") {
-    const guild = validateGuild(interaction);
-    await checkVoiceChannelValidity(interaction);
+    let guild: Guild;
+    let voiceChannel: VoiceBasedChannel;
+    try {
+      guild = validateGuild(interaction);
+      voiceChannel = await checkVoiceChannelValidity(interaction);
+    } catch (error) {
+      logger.error((error as Error).message);
+      return;
+    }
 
     const query = interaction.options.get("judul")?.value;
     if (typeof query !== "string") throw "Gagal nih process judulnya";
@@ -38,11 +73,8 @@ client.on("interactionCreate", async (interaction) => {
       metadata: {
         channel: interaction.channel,
       },
-      ytdlOptions: {
-        filter: "audioonly",
-        quality: "highestaudio",
-        highWaterMark: 1 << 25,
-        dlChunkSize: 0,
+      async onBeforeCreateStream(track, source, queue) {
+        return (await playdl.stream(track.url, { discordPlayerCompatibility: true })).stream;
       },
     });
 
@@ -52,7 +84,7 @@ client.on("interactionCreate", async (interaction) => {
 
     // verify vc connection
     try {
-      if (!queue.connection) await queue.connect(sender.voice.channel!);
+      if (!queue.connection) await queue.connect(voiceChannel);
     } catch {
       queue.destroy();
       await interaction.reply({
@@ -63,28 +95,34 @@ client.on("interactionCreate", async (interaction) => {
     }
 
     await interaction.deferReply();
-    const searchResult = await player.search(query, { requestedBy: sender });
-    if (searchResult.tracks.length === 0) {
+    const track = await player.search(query, { requestedBy: sender }).then((x) => x.tracks[0]);
+    if (!track) {
       await interaction.followUp({
         content: `Wadu, lagu **${query}** engga ketemu`,
       });
       return;
     }
 
-    const trackName = searchResult.tracks[0];
-    await queue.addTrack(trackName);
-    if (!queue.playing) queue.play();
+    await queue.addTrack(track);
+    logger.debug({ isplaying: queue.playing, isrunning: queue.playing === false });
+    if (queue.playing === false) await queue.play();
 
     await interaction.followUp({
-      content: `Ketemu ni **${trackName}**`,
+      content: `Ketemu ni **${track}**`,
     });
     return;
   }
 
   // stop music
   if (interaction.commandName === "stop") {
-    const guild = validateGuild(interaction);
-    await checkVoiceChannelValidity(interaction);
+    let guild: Guild;
+    try {
+      guild = validateGuild(interaction);
+      await checkVoiceChannelValidity(interaction);
+    } catch (error) {
+      logger.error((error as Error).message);
+      return;
+    }
 
     player.deleteQueue(guild);
 
@@ -96,7 +134,13 @@ client.on("interactionCreate", async (interaction) => {
 
   // skip music
   if (interaction.commandName === "skip") {
-    const queue = await getGuildQueue(interaction);
+    let queue: Queue | undefined;
+    try {
+      queue = await getGuildQueue(interaction);
+    } catch (error) {
+      logger.error((error as Error).message);
+      return;
+    }
     if (!queue || !queue.playing) {
       await interaction.reply({
         content: "Lagi gada lagu kamu skip gimana si",
@@ -118,6 +162,7 @@ client.on("interactionCreate", async (interaction) => {
     const queue = player.getQueue(guild);
     if (!queue || !queue.playing) {
       await interaction.reply({ content: "Isi playlist kosong ni" });
+      return;
     }
 
     const currentTrack = queue.current;
@@ -198,23 +243,25 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
-async function checkVoiceChannelValidity(interaction: CommandInteraction): Promise<void> {
+async function checkVoiceChannelValidity(interaction: CommandInteraction): Promise<VoiceBasedChannel> {
   const memberVoice = (interaction.member as GuildMember).voice;
   if (!interaction.inGuild()) throw Error("Not in guild");
   const guild = interaction.guild;
-  if (!memberVoice.channelId) {
+  if (!memberVoice.channelId || !memberVoice.channel) {
     await interaction.reply({
       content: "Ga di voice channel sayang",
       ephemeral: true,
     });
-    return;
+    throw Error("Member not in voice channel");
   }
   if (guild?.members.me?.voice.channelId && memberVoice.channelId !== guild.members.me.voice.channelId) {
     await interaction.reply({
       content: "Beda voice channel sayang",
       ephemeral: true,
     });
+    throw Error("Different voice channel");
   }
+  return memberVoice.channel;
 }
 
 function validateGuild(interaction: CommandInteraction): Guild {
